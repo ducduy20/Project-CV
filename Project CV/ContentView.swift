a4e70d01be137e40aa05aa4b10bfc0bbf9fc37ae
ducduy20 <86713122+ducduy20@users.noreply.github.com>
2022-03-20 09:47:24 +0700

    Initial Commit

---
+++ Project CV/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Project CV
+//
+//  Created by Tran Duc Duy on 20/03/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Project CV/Project_CVApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project_CVApp.swift
+//  Project CV
+//
+//  Created by Tran Duc Duy on 20/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct Project_CVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
